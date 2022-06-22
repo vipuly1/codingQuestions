da72a88bfa1967a5cd33513397bc728bd9b8e5e4
vipuly1 <yadavvipul797@gmail.com>
2022-06-22 20:38:58 +0530

    solved few question

---
+++ noOfJumps.js
@@ -0,0 +1,26 @@
+const arr = [2,3,0,1,4]
+const noOfJumps = (arr) => {
+    let jumpCount = 0
+    let i = 0
+    while (i < arr.length) {
+            i = i + arr[i]
+            jumpCount = jumpCount + 1
+
+            console.log(arr[i])
+            if (i >= arr.length) {
+                jumpCount = -1
+                return jumpCount
+            }
+            else if (i === arr.length - 1) {
+                return jumpCount
+            }
+            else if(arr[i] == 0){
+                jumpCount = jumpCount + 1
+                break
+            }
+            console.log("jump added", i)
+        }
+
+    return jumpCount
+}
+console.log("Total Jumps",noOfJumps(arr))
